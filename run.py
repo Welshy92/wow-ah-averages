702dd8cebf72b9be3a21726787055489760ea11d
Jacob Welsh <welshy92@outlook.com>
2022-12-08 14:33:20 +0000

    Add introduction and main functions

--- run.py
+++ run.py
@@ -12,8 +12,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('wow-averages')
 
-price = SHEET.worksheet("price")
+def introduction():
+    """
+    This prints an introductory statement and gives a brief explaination of how the
+    data should be entered by the user into the terminal.
+    """
+    print("Welcome to the WoW AH average price analyser.\n")
+    print("You will go through 8 popular items to sell, one by one.")
+    print("You will need to know the current lowest price of each item on the AH.")
+    print("When asked, please enter your price in Silver. Remember 1 Gold = 100 Silver.")
+    print("If the price has Copper in it, please round it to the nearest Silver.")
 
-data = price.get_all_values()
 
-print(data)
+def main():
+    """
+    This will run all the required program functions
+    """
+    introduction()
+
+
+main()
