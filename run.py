2bac05eb53a6e7794805e5fb0481a6ab7294e68d
Jacob Welsh <welshy92@outlook.com>
2022-12-10 13:46:13 +0000

    Add functions for remaining 6 items of data required

--- run.py
+++ run.py
@@ -41,6 +41,7 @@ def get_titansteel_data():
 
     return data_str
 
+
 def get_deviate_data():
     """
     Get price figure input for a Savory Deviate Delight from the user.
@@ -50,6 +51,108 @@ def get_deviate_data():
     """
     while True:
         data_str = input("Enter the price of a Savory Deviate Delight\n")
+
+        if validate_data(data_str):
+            print("Data is valid!")
+            break
+
+    return data_str
+
+
+def get_pygmy_data():
+    """
+    Get price figure input for a Pygmy Oil from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a whole number.
+    The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        data_str = input("Enter the price of a Pygmy Oil\n")
+
+        if validate_data(data_str):
+            print("Data is valid!")
+            break
+
+    return data_str
+
+
+def get_orb_data():
+    """
+    Get price figure input for a Frozen Orb from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a whole number.
+    The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        data_str = input("Enter the price of a Frozen Orb\n")
+
+        if validate_data(data_str):
+            print("Data is valid!")
+            break
+
+    return data_str
+
+
+def get_lotus_data():
+    """
+    Get price figure input for a Frost Lotus from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a whole number.
+    The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        data_str = input("Enter the price of a Frost Lotus\n")
+
+        if validate_data(data_str):
+            print("Data is valid!")
+            break
+
+    return data_str
+
+
+def get_illusion_data():
+    """
+    Get price figure input for a Illusion Dust from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a whole number.
+    The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        data_str = input("Enter the price of a Illusion Dust\n")
+
+        if validate_data(data_str):
+            print("Data is valid!")
+            break
+
+    return data_str
+
+
+def get_eternal_data():
+    """
+    Get price figure input for a Eternal Fire from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a whole number.
+    The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        data_str = input("Enter the price of a Eternal Fire\n")
+
+        if validate_data(data_str):
+            print("Data is valid!")
+            break
+
+    return data_str
+
+
+def get_frostweave_data():
+    """
+    Get price figure input for a Frostweave Cloth from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a whole number.
+    The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        data_str = input("Enter the price of a Frostweave Cloth\n")
 
         if validate_data(data_str):
             print("Data is valid!")
@@ -79,6 +182,12 @@ def main():
     introduction()
     get_titansteel_data()
     get_deviate_data()
+    get_pygmy_data()
+    get_orb_data()
+    get_lotus_data()
+    get_illusion_data()
+    get_eternal_data()
+    get_frostweave_data()
 
 
 main()
